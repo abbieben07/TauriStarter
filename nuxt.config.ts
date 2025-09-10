0f846d491c212283e04bfbb3b789de8b29b2e00d
Abraham Ben-Obaje <abbieben07@gmail.com>
2025-09-10 11:59:31 +0100

    chore: update Nuxt compatibility date to latest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: '2024-04-03',
+	compatibilityDate: 'latest',
 	devtools: { enabled: true },
 	vite: {
 		esbuild: {
